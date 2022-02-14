9506f52aabdb255f3053a9821afcc793175acc5e
Miko528 <mikolajboniecki2004@wp.pl>
2022-02-14 20:58:59 +0100

    Deleate add courses

--- src/Pages/Courses/Courses.tsx
+++ src/Pages/Courses/Courses.tsx
@@ -1,22 +1,12 @@
 import { ReactElement, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { saveCourse } from "../../redux/Courseslist";
 import { RootState } from "../../redux/Store";
 import { fetchCourses } from "../../redux/actions/CoursesActions";
 import { Link } from "react-router-dom";
 
 export const Courses = (): ReactElement => {
-  const [input, setInput] = useState<any | null>();
   const courses = useSelector((state: RootState) => state.courseslist.value);
   const dispatch = useDispatch();
-
-  const addCourses = () => {
-    dispatch(
-      saveCourse({
-        course: input,
-      })
-    );
-  };
 
   useEffect(() => {
     !courses.length && dispatch(fetchCourses());
@@ -24,16 +14,12 @@ export const Courses = (): ReactElement => {
 
   return (
     <div>
-      <div>
-        {courses &&
-          courses.map((el) => (
-            <Link to={""}>
-              <div key={el.id}>{el.id}</div>
-            </Link>
-          ))}
-      </div>
-      <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={addCourses}>add</button>
+      {courses &&
+        courses.map((el) => (
+          <Link to={""}>
+            <div key={el.id}>{el.id}</div>
+          </Link>
+        ))}
     </div>
   );
 };
